dbaa1c75391fb9a0eccd38d643735a3ec2c44df4
ChadDoomer <acarlisle@eagles.usi.edu>
2023-03-14 17:01:15 -0500

    Upload

---
+++ CS478 Project/Assets/Scripts/Tile.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour
+{
+    // creates reference to text component
+    private TextMeshProUGUI text;
+
+    // letter property for the tile
+    public char letter { get; private set; }
+
+    // called automatically by Unity, establishes reference to text component
+    private void Awake()
+    {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    // sets the letter that shows up in the tile
+    public void SetLetter(char letter)
+    {
+        this.letter = letter;
+        text.text = letter.ToString();
+    }
+    
+}
